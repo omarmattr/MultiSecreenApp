cdf1d9d6416c8e3fdc31a3151e476c7204ec2c9f
omarmattr <omarabomattr@gmail.com>
2021-04-06 06:10:29 -0700

    Initial Commit

---
+++ MultiSecreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiSecreenApp
+//
+//  Created by omar mattr on 06/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
